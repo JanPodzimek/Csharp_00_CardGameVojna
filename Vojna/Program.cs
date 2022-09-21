d8a7e4aa368affbf6a5da0ee30ab589677011465
Jan Podzimek <podzimek.jan@seznam.cz>
2022-09-21 22:06:49 +0200

    redundant lines removed

--- Vojna/Program.cs
+++ Vojna/Program.cs
@@ -1,13 +1,11 @@
 ﻿namespace Vojna;
 public class Program {
     public static void Main(string[] args) {
-
         Player player1 = new Player();
         Player player2 = new Player();
         Dice dice = new Dice();
         Table table = new Table(dice, player1, player2);
         table.Game();
-
     }
 }
 
